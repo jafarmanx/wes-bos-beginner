b55013ccd08373f95a1d56edfb3ccaedce0f9082
jafarmanx <ahsenjaffery007@gmail.com>
2022-03-18 23:27:09 -0500

    Made changes that I dont remember

--- outside-the-box/outside.js
+++ outside-the-box/outside.js
@@ -1,7 +1,12 @@
 const cardButtons = document.querySelectorAll('.card button');
+const modalInner = document.querySelector('.modal-inner');
 
-function handleCardButtonClick() {
-  console.log('its ya boy');
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest('.card');
+  const imgSrc = card.querySelector('img').src;
+  const desc = card.dataset.description;
+  modalInner.innerHTML = `<img src="${imgSrc.replace('200')}" alt""/`;
 }
 
 cardButtons.forEach((button) =>
