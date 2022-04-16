cbd3d37a664ab1e4c4c0b412f90d586d3bb0982c
Ahsen Jaffery <AhsenJaffery007@gmail.com>
2022-04-16 02:26:13 -0500

    Additonal Changes for testing git commit on new machine

--- types.js
+++ types.js
@@ -3,3 +3,7 @@ const name = 'ahsen';
 const middle = 'shahid';
 const last = 'jaffery';
 const something = 'change to file';
+const name1 = 'ahsen';
+const middle1 = 'shahid';
+const last1 = 'jaffery';
+const something1 = 'change to file';
