9521037f6611c30a39b4b1c8b5375d132ac32529
Ahsen Jaffery <AhsenJaffery007@gmail.com>
2022-04-17 18:34:27 -0500

    made changes to outside per wes boss

--- outside-the-box/outside.js
+++ outside-the-box/outside.js
@@ -1,14 +1,45 @@
 const cardButtons = document.querySelectorAll('.card button');
 const modalInner = document.querySelector('.modal-inner');
+const modalOutter = document.querySelector('.modal-outer');
 
 function handleCardButtonClick(event) {
   const button = event.currentTarget;
   const card = button.closest('.card');
   const imgSrc = card.querySelector('img').src;
   const desc = card.dataset.description;
-  modalInner.innerHTML = `<img src="${imgSrc.replace('200')}" alt""/`;
+  const name = card.querySelector('h2').textContent;
+  console.log(imgSrc);
+  modalInner.innerHTML = `
+    <img 
+      src="${imgSrc.replace(/300/gi,'600')}" 
+      alt="${name}"
+    />
+    <p>${desc}</p>
+    `;
+  // show the modal
+  modalOutter.classList.add('open')
+  console.log(modalOutter.querySelector('img').src);
 }
 
 cardButtons.forEach((button) =>
   button.addEventListener('click', handleCardButtonClick)
 );
+
+function closeModal() {
+  modalOutter.classList.remove('open')
+} 
+
+modalOutter.addEventListener('click', function(event){
+  const isOutside = !event.target.closest('.modal-inner');
+  console.log(isOutside);
+  if(isOutside)
+  {
+    closeModal()    
+  }
+});
+
+window.addEventListener('keydown', event=>{
+  if (event.key === 'Escape'){
+    closeModal()
+  }
+});
