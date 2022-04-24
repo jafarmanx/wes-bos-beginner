c951f8af92463e4164f4f46d3da1ba8ceee628fb
Ahsen Jaffery <AhsenJaffery007@gmail.com>
2022-04-23 22:03:45 -0500

    Update with example of scroll events

--- scroll-events/scrollevent.js
+++ scroll-events/scrollevent.js
@@ -1,1 +1,21 @@
-console.log("it works!")
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('button');
+
+function obCallback(payload){
+     if (payload[0].intersectionRatio === 1) {
+        button.classList.add('on');
+        button.disabled = false;
+        // un observe it after running to stop it from constently triggering.
+        ob.unobserve(terms.lastElementChild);
+    }
+  
+}
+const ob = new IntersectionObserver(obCallback, {root:terms, threshold:1});
+ob.observe(terms.lastElementChild);
+
+
+// terms.addEventListener('scroll', function(e) {
+//     console.log(e.currentTarget.scrollTop);
+//     console.log(e.currentTarget.scrollHeight);
+// });
